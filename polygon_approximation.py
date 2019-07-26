72c3e7ac79c2149fdc074bee363322c3156760fd
Arghadeep Mazumder <arghadeep25@gmail.com>
2019-07-26 17:58:55 +0200

    Polygon extraction file added

---
+++ polygon_approximation.py
@@ -0,0 +1,35 @@
+import os
+import cv2
+import sys
+import numpy as np
+
+
+path = sys.argv[1]
+filename = os.path.basename(path)
+
+input_image = cv2.imread(path)
+image = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
+image = np.uint8(image)
+image = np.expand_dims(image,3)
+
+print('Image Shape:',image.shape)
+
+contours, _ = cv2.findContours(image, 
+                                  cv2.RETR_EXTERNAL, 
+                                  cv2.CHAIN_APPROX_SIMPLE)
+
+print('Number of Shapes: ',len(contours))
+
+for contour in contours:
+    eps = 0.01 * cv2.arcLength(contour, True)
+    shapes = cv2.approxPolyDP(contour, eps, True)
+    output = cv2.drawContours(input_image, 
+                                    [shapes], 
+                                    0, (0,0,255), 3)
+
+print("Output Shape: ",output.shape)
+save_dir = 'results/polygon/'+filename
+cv2.imwrite(save_dir, output)
+cv2.imshow('Output',output)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
